031f54616c6c4712e08861cb7f37e816854aa101
Zarhkoh <zarhkoh@hotmail.fr>
2021-06-12 02:29:59 +0200

    ajout de la fonction 'getReminder' dans le builder

--- src/builders/reminder.builder.ts
+++ src/builders/reminder.builder.ts
@@ -1,16 +1,28 @@
 const db = require('../db_init');
 const Reminder = require('../models').Reminder;
 
-module.exports.setReminder = function(reminder) {
-    console.log("builder atteint");
+module.exports.setReminder = function (reminder) {
      return new Promise(async(resolve, reject) => {
          try {
              const result = await Reminder.create(reminder);
-             
-             console.log("enregistré?");
              resolve(result);
          } catch (err) {
              reject(err);
          }
      });
- }
+}
+
+module.exports.getReminderByDateTime = function (dateTime) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const result = await Reminder.findAll({
+                where: {
+                    dateRappel: dateTime
+                }
+            });
+            resolve(result);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
